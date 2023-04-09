394cc921154476c92fcdfa7625cc7a54154913c8
Ausar <464270342@qq.com>
2023-04-09 22:37:47 +0800

    删除了汇编中的r0
    Signed-off-by: Ausar <464270342@qq.com>

--- myentry.c
+++ myentry.c
@@ -10,7 +10,7 @@ void __attribute__((naked)) jump_to_target(void)
     "ldr pc, %0\n\t" // 将origin_entry的地址加载到r0寄存器中
     : 
     : "m" (globleVar.origin_entry) // 输入操作数是origin_entry的内存地址
-    : "r0" // 破坏了r0寄存器
+    : 
     );
 }
 
